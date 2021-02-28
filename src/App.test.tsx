bd1377cc3758a51fe042160c2c45d76943c4815d
Xinghua <xinghua24@gmail.com>
2021-02-28 18:52:05 -0500

    add Test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React, { ReactElement } from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
+import { MemoryRouter, Router } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+function renderWithProviders(ui: any) {
+  return render(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={["/"]}>{ui}</MemoryRouter>
+    </Provider>
+  );
+}
+
+test("renders welcome text", async () => {
+  renderWithProviders(<App />);
+  const signInButton = screen.getByText(/signin/i);
+  await screen.findByText(/Welcome to Xing's Bookmark/i);
 });
